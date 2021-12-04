e838152cae2c42524b42a0ac9e73bc3b70f52ce1
Autumn-repeat <zzq370381990@163.com>
2021-12-04 15:58:14 +0800

    initial project version

---
+++ Language/TestFor.java
@@ -0,0 +1,15 @@
+package container;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestFor {
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3};
+        System.out.println(Arrays.toString(ints));
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
